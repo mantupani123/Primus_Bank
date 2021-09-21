e4eabd98ac27316b3692e49ddfa0dafb3fd6aadd
mantupani123 <mantupani147@gmail.com>
2021-09-21 20:03:29 +0530

    java script executor

--- src/June9/JavaScript1.java
+++ src/June9/JavaScript1.java
@@ -11,7 +11,19 @@ public class JavaScript1  {
 	driver.manage().window().maximize();
 	Thread.sleep(5000);
 JavascriptExecutor js = (JavascriptExecutor)driver;
-js.executeScript("window.location=",   "https://amazon.in");
+driver.get("https://www.bharatmatrimony.com/");
+Thread.sleep(5000);
+String strtitle=js.executeScript("return document.title").toString();
+System.out.println("Page title::"+strtitle);
+System.out.println("Page title length::"+strtitle.length());
+String strlurl=js.executeScript("return document.URL").toString();
+System.out.println("url is::"+strlurl);
+System.out.println("url lenght is::"+strlurl.length());
+String domain=js.executeScript("return document.domain").toString();
+System.out.println("domain name::"+domain);
+System.out.println("domain length is::"+domain.length());
+Thread.sleep(5000);
+driver.close();
 	}
 
 }
